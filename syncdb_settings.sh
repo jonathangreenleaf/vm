d56058f23e5febe389a5cdcd53bf1f8d0bea0a3b
debian <debian@debian.(none)>
2014-10-10 11:44:54 -0400

    handle db/settings outside git

---
+++ syncdb_settings.sh
@@ -0,0 +1,2 @@
+scp /home/debian/Desktop/skillshare/src/db.sqlite3 super_jgreenleaf@jonathangreenleaf.com:/home/super_jgreenleaf/vm/src/db.sqlite3
+scp /home/debian/Desktop/skillshare/src/mvp_landing/settings.py super_jgreenleaf@jonathangreenleaf.com:/home/super_jgreenleaf/vm/src/mvp_landing/settings.py
