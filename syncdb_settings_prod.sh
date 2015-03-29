6c879779a536238dd3208fb555f5112ad563cf02
jonathan greenleaf <jonathangreenleaf@gmail.com>
2015-03-29 10:47:02 -0400

    more prod config

---
+++ syncdb_settings_prod.sh
@@ -0,0 +1,2 @@
+scp /home/debian/Desktop/skillshare/src/db.sqlite3 super_jgreenleaf@jonathangreenleaf.com:/home/super_jgreenleaf/prod/vm/src/db.sqlite3
+scp /home/debian/Desktop/skillshare/src/mvp_landing/settings.py super_jgreenleaf@jonathangreenleaf.com:/home/super_jgreenleaf/prod/vm/src/mvp_landing/settings.py
